00c73d6d612dc2ab86e1756e09782fc393eaec9d
l76000 <colakovicl035@gmail.com>
2025-11-27 00:34:47 +0100

    Update sve.js

--- api/sve.js
+++ api/sve.js
@@ -5,7 +5,7 @@ export default function handler(req, res) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sva Vozila - BeoMapa</title>
+    <title>Sva Vozila</title>
     
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
     
